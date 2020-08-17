fb40c38564def252e75d11a5b20d219160188231
SebastiánEPH <sebastianeph99@gmail.com>
2020-08-17 09:29:31 -0500

    update new projects

---
+++ src/controlador/cMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import vista.vPresentacion;
+import vista.vPrincipal;
+
+/**
+ *
+ * @author Josue
+ */
+public class cMain {
+    public static void main(String [ ] args ) {
+    
+  //  vPresentacion vista=new vPresentacion();
+    //vista.setVisible(true);
+      vPrincipal vista=new vPrincipal();
+    vista.setVisible(true);
+    }
+}
